44768a909736d326442a433ec82894b55d114f3e
lofitemi9 <temiloluwashokubi@gmail.com>
2025-11-11 13:12:07 -0500

    updated file structure a bit

--- Georgian_Comp/PHP_Lessons/CSS-PHP-Project/templates/footer.php
+++
@@ -1,6 +0,0 @@
-        <footer>
-            <h6>&copy; <?php echo date("Y"); ?> Jeremy McCulley</h6>
-        </footer>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    </body>
-</html>

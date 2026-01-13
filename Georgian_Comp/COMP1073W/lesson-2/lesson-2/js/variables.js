96fafd72e8f6644d9e739db9c5f3d2841c9335ea
lofitemi9 <temiloluwashokubi@gmail.com>
2026-01-13 10:51:33 -0500

    Completed numbers

--- Georgian_Comp/COMP1073W/lesson-2/lesson-2/js/variables.js
+++ Georgian_Comp/COMP1073W/lesson-2/lesson-2/js/variables.js
@@ -1,3 +1,58 @@
+/* STEP 1: Declare variables
+		Use a-z, 0-9, A-Z and underscores, avoiding reserved names
+		All program variables are generally declared at the beginning */
+        let myName;
+        let myAge;
+        let string;
+        let para = document.querySelector('p')
+
+		/* STEP 2: Initialize variables
+		Don't confuse a variable with no value with one that doesn't exist at all */
+        myName = 'Shivali Dhaka';
+        myAge = 30;
+
+		/* STEP 3: Updating variables
+		Once a variable has been declared, this can be done any time */
+        myName = 'Shivi';
+        myAge = 31;
+        string - myName + ' ' + myAge;
+        para.textContent = string;
+
+		// STEP 4: Variable types (use typeof in the console)
+		// Numbers
+        let numCourse = 0;
+        para.textContent = numCourse;
+        console.log(typeof(numCourse));
+
+
+		// Strings (use '' or "")
+        let stringCourse = 'Client Side JS';
+        para.textContent += stringCourse;
+
+		// Booleans
+        let iAmAlive = true;
+        let compare = 5 < 2;
+        para.textContent = iAmAlive;
+        console.log('The result of comparison is: ', compare);
+
+		// Arrays
+        let myArray = ['Sarah','Steve','Shivi'];
+        para.textContent = myArray[1];
+
+        let myArray2 = [20,30,40];
+        para.textContent = myArray[2];
+		
+		// Objects
+        let myDog = {fName: 'Simba', breed:'staffy', color:'brown'};
+        console.log(myDog.fName);
+
+		// STEP 5: Typing (JavaScript is a loosely-typed language)
+        let myNumber = 300;
+        console.log(typeof(myNumber));
+        myNumber = false;
+        console.log(typeof(myNumber));
+
+
 //Difference between Var and Let
 
 //1. Scope

7b6727a7f0289e46373b591652eca5957b9c2210
lofitemi9 <temiloluwashokubi@gmail.com>
2026-01-18 21:27:56 -0500

    Uploaded corrected lesson 2

--- Georgian_Comp/COMP1073W/lesson-2/js/variables.js
+++ Georgian_Comp/COMP1073W/lesson-2/js/variables.js
@@ -4,7 +4,7 @@
         let myName;
         let myAge;
         let string;
-        let para = document.querySelector('p')
+        let para = document.querySelector('p');
 
 		/* STEP 2: Initialize variables
 		Don't confuse a variable with no value with one that doesn't exist at all */
@@ -13,21 +13,20 @@
 
 		/* STEP 3: Updating variables
 		Once a variable has been declared, this can be done any time */
-        myName = 'Shivi';
+        myName ='shivi';
         myAge = 31;
-        string - myName + ' ' + myAge;
+        string = myName +' ' + myAge;
         para.textContent = string;
 
 		// STEP 4: Variable types (use typeof in the console)
 		// Numbers
-        let numCourse = 0;
+        let numCourse = 6;
         para.textContent = numCourse;
         console.log(typeof(numCourse));
 
-
 		// Strings (use '' or "")
         let stringCourse = 'Client Side JS';
-        para.textContent += stringCourse;
+        para.textContent = stringCourse;
 
 		// Booleans
         let iAmAlive = true;
@@ -36,65 +35,70 @@
         console.log('The result of comparison is: ', compare);
 
 		// Arrays
-        let myArray = ['Sarah','Steve','Shivi'];
-        para.textContent = myArray[1];
+        let myArray= ['Sarah','Steeve','Shivi'];
+        para.textContent =myArray[1];
 
-        let myArray2 = [20,30,40];
-        para.textContent = myArray[2];
+        let myArray2= [20,30,40];
+        para.textContent =myArray[2];
 		
 		// Objects
-        let myDog = {fName: 'Simba', breed:'staffy', color:'brown'};
+        let myDog = {fName: 'Simba',breed:'staffy',color:'brown'};
         console.log(myDog.fName);
 
+
 		// STEP 5: Typing (JavaScript is a loosely-typed language)
-        let myNumber = 300;
+        let myNumber = 500;
         console.log(typeof(myNumber));
         myNumber = false;
         console.log(typeof(myNumber));
 
-
-//Difference between Var and Let
-
+//Difference between var and let
 //1. Scope
-function testscope() {
-    if(true) {
-        var a = 10; //function-scoped variable
-    }
-    console.log(a);
+        function testScope(){
+            if(true){
+                var a = 10;//function-scoped
+            }
+            console.log(a);
 
-    if(true) {
-        let b = 20;
-        console.log(b); //block-scoped variable
-    }
-    // console.log(b);
-}
+             if(true){
+                let b = 20;//block-scoped
+                 console.log(b);
+            }
+            //console.log(b);
 
-testscope(); 
+        }
 
-//2. Re-declaration
-function demo(){
-    //Hoisting with var
-    console.log('value of x before declaration', x)
-    var x = 10;
-
-    //Hoisting with let
-    console.log('value of y before declaration', y)
-    let y = 11;
-
-    //Redeclaration with var
-    var a = 15;
-    var a = 20;
-    console.log("Redeclared var a: ", a);
-
-    // Redeclaration with let
-
-    let b = 10;
-    // let b = 20;
-    console.log('Redeclared let b: ', b);
-        console.log(c);
-        const c = 10;
-        // c = 20;
+        testScope();
 
 
-}
-demo();
+        //2.Re-declartion and hoisitng
+
+        function demo(){
+            //Hoisting with var
+            console.log('value of x before declataion',x);
+            var x = 10;
+
+             //Hoisting with let
+            //console.log('value of y before declataion',y);
+            let y = 11;
+            
+
+
+            //Redeclaration with var
+            var a = 15;
+            var a = 20;
+            console.log('Redeclared var a: ',a);
+
+            //Redeclaration with let
+
+            let b = 10;
+             b = 20;
+            console.log('Redeclared let b: ',b);
+                 
+                const c = 10;
+                //c = 20;
+                console.log(c);
+               
+
+        }
+        demo();

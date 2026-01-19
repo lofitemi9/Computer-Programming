ccc057edf33c343678f0b1af12df4bd79962525b
lofitemi9 <temiloluwashokubi@gmail.com>
2026-01-18 22:22:57 -0500

    created the pet age system that either increase or decreases the pets age when the corresponding button is clicked

--- Georgian_Comp/COMP1073W/lesson-2/lab-1/js/pet.js
+++ Georgian_Comp/COMP1073W/lesson-2/lab-1/js/pet.js
@@ -19,4 +19,27 @@ let moodArray = ['Happy', 'Sleepy', 'Exicted', 'Grumpy'];
 //Select a random mood from the array
 mood = moodArray[Math.floor(Math.random() * moodArray.length)]
 
+// creating the petAge system to either increase or decrease the pets Age
+    // assigning the button to values
+    const increasePetAge = document.querySelector('#increaseAgeField');
+    const decreasePetAge = document.querySelector('#decreaseAgeField');
+
+    // conditional system to check if button has been clicked or not
+    increasePetAge.addEventListener("click", function () {
+        // updating age to increase by 1 when button clicked
+        petAge += 1;
+        console.log(petAge)
+
+    });
+        // updating age to decrease by 1 when button clicked
+    decreasePetAge.addEventListener("click", function () {
+        if (petAge > 0) {
+            petAge -= 1;
+            console.log(petAge) } 
+    })
+
+// Creating Basic UI to show information content
+
+
+
 

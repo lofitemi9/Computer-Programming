f83576188b751e76416b1ac10999a460b89eff32
lofitemi9 <temiloluwashokubi@gmail.com>
2026-01-18 21:41:42 -0500

    Initialized basic arrays required for the pet system

--- Georgian_Comp/COMP1073W/lesson-2/lab-1/js/pet.js
+++ Georgian_Comp/COMP1073W/lesson-2/lab-1/js/pet.js
@@ -6,3 +6,17 @@ let isHungry;
 let favouriteActivities;
 let mood;
 
+
+// Initialize Array
+let petArray = ['Dog', 'Cat', 'Fish'];
+
+// Select a random petAge between 1 and 15
+petAge = Math.floor(Math.random() * 15) + 1
+
+// Initialize moodArray
+let moodArray = ['Happy', 'Sleepy', 'Exicted', 'Grumpy'];
+
+//Select a random mood from the array
+mood = moodArray[Math.floor(Math.random() * moodArray.length)]
+
+

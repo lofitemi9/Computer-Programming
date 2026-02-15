02bb42ae23db6ed70229fd92f919d83ac9433e2c
lofitemi9 <temiloluwashokubi@gmail.com>
2026-02-14 19:44:58 -0500

    linked the js and added some button click handlers to test if they work in the console

---
+++ Georgian_Comp/COMP1073W/assignment1/js/script.js
@@ -0,0 +1,11 @@
+const btn1 = document.getElementById("btn1");
+const btn2 = document.getElementById("btn2");
+const btn3 = document.getElementById("btn3");
+const btn4 = document.getElementById("btn4");
+const btn5 = document.getElementById("btn5");
+
+btn1.addEventListener("click", () => console.log("btn1 clicked"));
+btn2.addEventListener("click", () => console.log("btn2 clicked"));
+btn3.addEventListener("click", () => console.log("btn3 clicked"));
+btn4.addEventListener("click", () => console.log("btn4 clicked"));
+btn5.addEventListener("click", () => console.log("btn5 clicked"));
